b082c5c0edb4903a1df485372f9a7b096029428a
Rodrigo Quispe <rodrigo.quispe@rappi.com>
2021-09-10 08:02:11 -0500

    Update MainVerticle.kt

--- src/main/kotlin/com/rodrigo/consumer_azure_service_bus/MainVerticle.kt
+++ src/main/kotlin/com/rodrigo/consumer_azure_service_bus/MainVerticle.kt
@@ -4,40 +4,26 @@ import com.azure.messaging.servicebus.ServiceBusClientBuilder
 import com.azure.messaging.servicebus.ServiceBusErrorContext
 import com.azure.messaging.servicebus.ServiceBusException
 import com.azure.messaging.servicebus.ServiceBusProcessorClient
-import com.azure.messaging.servicebus.ServiceBusReceivedMessage
 import com.azure.messaging.servicebus.ServiceBusReceivedMessageContext
 import io.vertx.core.AbstractVerticle
 import java.util.concurrent.TimeUnit
-import java.util.function.Consumer
 
 
 class MainVerticle : AbstractVerticle() {
 
+    companion object {
+        const val CONNECTION_STRING: String =
+            "Endpoint=sb://<HOST_ENDPOINT>;SharedAccessKeyName=<ACCESS_KEY_NAME>;SharedAccessKey=<ACCESS_KEY>"
+        const val QUEUE_NAME: String = "<QUEUE_NAME>"
+    }
+
     override fun start() {
-        val onMessage: Consumer<ServiceBusReceivedMessageContext> =
-            Consumer<ServiceBusReceivedMessageContext> { context ->
-                val message: ServiceBusReceivedMessage = context.message
-                println("Processing message. Sequence #: ${message.sequenceNumber}. Contents: ${message.body}")
-            }
-
-        val onError =
-            Consumer { context: ServiceBusErrorContext ->
-                println("Error when receiving messages from namespace: ${context.fullyQualifiedNamespace}. Entity: ${context.entityPath}")
-                if (context.exception is ServiceBusException) {
-                    val exception = context.exception as ServiceBusException
-                    println("Error source: ${context.errorSource}, reason ${exception.reason}")
-                } else {
-                    println("Error occurred: ${context.exception}")
-                }
-            }
-
-        // Create an instance of the processor through the ServiceBusClientBuilder
         val processorClient: ServiceBusProcessorClient = ServiceBusClientBuilder()
-            .connectionString("Endpoint=sb://<HOST_ENDPOINT>;SharedAccessKeyName=<ACCESS_KEY_NAME>;SharedAccessKey=<ACCESS_KEY>")
+            .connectionString(CONNECTION_STRING)
             .processor()
-            .queueName("<QUEUE_NAME>")
-            .processMessage(onMessage)
-            .processError(onError)
+            .queueName(QUEUE_NAME)
+            .processMessage { receivedMessageContext -> onMessage(receivedMessageContext) }
+            .processError { errorContext -> onError(errorContext) }
             .buildProcessorClient()
 
         println("Starting the processor")
@@ -48,5 +34,18 @@ class MainVerticle : AbstractVerticle() {
         processorClient.close()
 
         super.start()
+    }
+
+    private fun onMessage(context: ServiceBusReceivedMessageContext) {
+        println("Processing message. Sequence #: ${context.message.sequenceNumber}. Contents: ${context.message.body}")
+    }
+
+    private fun onError(context: ServiceBusErrorContext) {
+        if (context.exception is ServiceBusException) {
+            val exception = context.exception as ServiceBusException
+            println("Error source: ${context.errorSource}, reason ${exception.reason}")
+        } else {
+            println("Error occurred: ${context.exception}")
+        }
     }
 }
