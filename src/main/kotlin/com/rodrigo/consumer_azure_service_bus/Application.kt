6c5fc4415690cbc871a8552f6611f3c8865a6488
Rodrigo Quispe <rodrigo.quispe@rappi.com>
2021-09-09 22:57:05 -0500

    load files

--- src/main/kotlin/com/rodrigo/consumer_azure_service_bus/Application.kt
+++ src/main/kotlin/com/rodrigo/consumer_azure_service_bus/Application.kt
@@ -8,11 +8,9 @@ class Application {
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
-            println("Init Vertx")
             val vertex = Vertx.vertx()
-            println("Deploy Main Verticle")
-
             val mainVerticle = MainVerticle()
+
             vertex.deployVerticle(mainVerticle, DeploymentOptions().apply {})
 
             println("Application Success Running")
